ce0a546c55a9522a3ebf3f1bc051971de32017ae
Keeghaan <lemaildejo@gmail.com>
2023-03-19 21:25:44 +0100

    mariadb ok

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -5,9 +5,9 @@ if [ ! -f /var/www/wordpress/wp-config.php ]; then
 	sleep 10
 
 	wp config create --allow-root \
-		--dbname=$SQL_DATABASE \
-		--dbuser=$SQL_USER \
-		--dbpass=$SQL_PASSWORD \
+		--dbname=$MYSQL_DATABASE \
+		--dbuser=$MYSQL_USER \
+		--dbpass=$MYSQL_PASSWORD \
 		--dbhost=mariadb:3306 \
 		--path='/var/www/wordpress'
 
